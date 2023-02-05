2f24361655bd3c4dca4a3f8b108e6a001bd3eea2
Ketil Malde <ketil@malde.org>
2023-02-05 20:31:39 +0100

    Add parsing test, and update test results

--- test.sh
+++ test.sh
@@ -6,8 +6,9 @@ while read sum cmd; do
     check=$($cmd | sha1sum | cut -f1 -d' ')
     if [ $check == $sum ]; then echo "passed."; else echo "FAILED!"; status=255; fi
 done <<EOF
-7c16489da9b182882d4722643e1394e8abfa183d python3 stracks.py -s --track=0 tests/stereo*
-e4d3070d277257fb8342f04f0da8aa4b5b273128 python3 stracks.py tests/lab2
+a2e9031bb4695be8d10167359c5d4d2e22527bb2 python3 parser.py data/L.csv
+e7bd2891fb15d75087cce4fed7b166d024199dba python3 stracks.py -s --track=0 tests/stereo*
+4ed3070d277257fb8342f04f0da8aa4b5b273128 python3 stracks.py tests/lab2
 EOF
 
 exit $status
