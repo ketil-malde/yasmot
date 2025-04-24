8a303897116399b45e5226335b7691eae0e6be66
Ketil Malde <ketil@malde.org>
2025-04-24 10:29:45 +0200

    Update test checksums with updated max_age tracking

--- test.sh
+++ test.sh
@@ -34,9 +34,9 @@ while read sum cmd; do
 	<<EOF
 b198fb325a02cd2f38bbddaa01fe76ac0f2a745f python3 -m src.yasmot.parser tests/L.csv
 ed53fccebf70e9b200b48e94343e5bd14290fc31 python3 -m src.yasmot.main -s --no-track --shape 1228,1027 tests/stereo1_Left.csv tests/stereo1_Right.csv
-a3aae3bacbfbd418d243f0a7e09bf92dae9eab9a python3 -m src.yasmot.main -s --shape 1228,1027 tests/stereo1_Left.csv tests/stereo1_Right.csv
+dd1c1a9132ab9d337182a30b9cccc9269e378c7b python3 -m src.yasmot.main -s --shape 1228,1027 tests/stereo1_Left.csv tests/stereo1_Right.csv
 1e856023ea79bc61decdb9499bb4f1e207b775eb python3 -m src.yasmot.main tests/lab2
-8c48b6ece4db0ea5d6925954e95214cf89fbaf22 python3 -m src.yasmot.main --max_age 2 --time_pattern frame_\{:d\}.txt tests/lab2
+fe37f2b601046395e39f8a12f240ef3234f0d49d python3 -m src.yasmot.main --max_age 2 --time_pattern frame_\{:d\}.txt tests/lab2
 2a14b4639ce0f30a3b6ab6b6f3084ed2cb8f455a python3 -m src.yasmot.main tests/error3.csv
 ce0f1f7b14f06fe3a949eec98c9923e8265fb308 python3 -m src.yasmot.main tests/lab2 --interpolate
 64f0161579349b6e7a431e8aab69807bf18e7a18 python3 -m src.yasmot.main -c tests/consensus/y8x*
