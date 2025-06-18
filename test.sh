8f1053d705c18f2e2270f5895d251d57d1ddcdcc
Ketil Malde <ketil@malde.org>
2025-06-18 16:15:25 +0200

    Update test checksums to account for header changes (& save outputs)

--- test.sh
+++ test.sh
@@ -19,10 +19,12 @@ else
     pip install .
 fi
 
+mkdir -p .tests
+
 status=0
 while read sum cmd; do
     echo -n "$cmd ... "
-    check=$($cmd | sha1sum | cut -f1 -d' ')
+    check=$($cmd | tee .tests/$sum.out | sha1sum | cut -f1 -d' ')
     if [ $check == $sum ]; then
 	echo "passed."
     else
@@ -33,13 +35,13 @@ while read sum cmd; do
     done\
 	<<EOF
 b198fb325a02cd2f38bbddaa01fe76ac0f2a745f python3 -m src.yasmot.parser tests/L.csv
-ed53fccebf70e9b200b48e94343e5bd14290fc31 python3 -m src.yasmot.main -s --no-track --shape 1228,1027 tests/stereo1_Left.csv tests/stereo1_Right.csv
-dd1c1a9132ab9d337182a30b9cccc9269e378c7b python3 -m src.yasmot.main -s --shape 1228,1027 tests/stereo1_Left.csv tests/stereo1_Right.csv
-1e856023ea79bc61decdb9499bb4f1e207b775eb python3 -m src.yasmot.main tests/lab2
-fe37f2b601046395e39f8a12f240ef3234f0d49d python3 -m src.yasmot.main --max_age 2 --time_pattern frame_\{:d\}.txt tests/lab2
-2a14b4639ce0f30a3b6ab6b6f3084ed2cb8f455a python3 -m src.yasmot.main tests/error3.csv
-ce0f1f7b14f06fe3a949eec98c9923e8265fb308 python3 -m src.yasmot.main tests/lab2 --interpolate
-64f0161579349b6e7a431e8aab69807bf18e7a18 python3 -m src.yasmot.main -c tests/consensus/y8x*
+3f0be6b6443913886d5a2d5a59692356149cec0b python3 -m src.yasmot.main -s --no-track --shape 1228,1027 tests/stereo1_Left.csv tests/stereo1_Right.csv
+68842a31c5b80add4ba6904e9b1d9ddff9afb68c python3 -m src.yasmot.main -s --shape 1228,1027 tests/stereo1_Left.csv tests/stereo1_Right.csv
+fb205f3406584f58f9120680c08a7df34ced5bd0 python3 -m src.yasmot.main tests/lab2
+d7fad65ece11c6b66033b26491c2d2815d746758 python3 -m src.yasmot.main --max_age 2 --time_pattern frame_\{:d\}.txt tests/lab2
+efaa15533e478ddf4db1fda103d1860f329561a4 python3 -m src.yasmot.main tests/error3.csv
+ed08e7453c4031f9e798a98f963239833e270dd5 python3 -m src.yasmot.main tests/lab2 --interpolate
+8be0fc9d8512c56a0a36a16f01c46f77bc5616fb python3 -m src.yasmot.main -c tests/consensus/y8x*
 EOF
 
 exit $status
